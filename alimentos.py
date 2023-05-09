0dd45538b6a63c4a2f29bbb88ef449ed194e670f
Víctor Santocildes Prado <victorsantocildes@opendeusto.es>
2023-05-09 18:56:47 +0200

    nuevos alimentos

--- alimentos.py
+++ alimentos.py
@@ -8,4 +8,5 @@ alimentos = [
     {"nombre":"corn flakes", "nutriscore":"b", "alergenos":"gluten, frutos secos, cacahuetes, soja, sesamo"},
     {"nombre":"yogur natural", "nutriscore":"a", "alergenos":"leche"},
     {"nombre":"yogur griego", "nutriscore":"c", "alergenos":"leche"},
+    {"nombre":"nocilla", "nutriscore":"e", "alergenos":"leche, soja, frutos secos"},
 ]
