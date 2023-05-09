4c84310e5dc95a0b2c3f00b6fcff50f3ac8ae273
MarioCervi <mariocervilla@opendeusto.es>
2023-05-09 19:13:51 +0200

    Union de bbdd

--- alimentos.py
+++ alimentos.py
@@ -5,4 +5,8 @@ alimentos = [
     {"nombre":"avecrem de verdura", "nutriscore":"d", "alergenos":"huevos, soja, apio"},
     {"nombre":"cocacola zero", "nutriscore":"b", "alergenos":""},
     {"nombre":"cocacola", "nutriscore":"e", "alergenos":""},
+    {"nombre":"corn flakes", "nutriscore":"b", "alergenos":"gluten, frutos secos, cacahuetes, soja, sesamo"},
+    {"nombre":"yogur natural", "nutriscore":"a", "alergenos":"leche"},
+    {"nombre":"yogur griego", "nutriscore":"c", "alergenos":"leche"},
+    {"nombre":"nocilla", "nutriscore":"e", "alergenos":"leche, soja, frutos secos"},
 ]
