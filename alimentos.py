8466ff78ad916ae8caeb20e0891537ff3e2863f5
MarioCervi <mariocervilla@opendeusto.es>
2023-05-09 16:01:35 +0200

    Estructura de ejemplo de la base de datos

---
+++ alimentos.py
@@ -0,0 +1,8 @@
+# Gluten, Cacahuetes, Crustaceos, Frutos secos, Moluscos, Soja, Pescado, Sesamo, Huevo, Altramuces, Apio, Leche, Mostaza, Anhídrido sulfuroso
+alimentos = [
+    {"nombre":"agua", "nutriscore":"a", "alergenos":""},
+    {"nombre":"nutella", "nutriscore":"e", "alergenos":"leche, soja, frutos secos"},
+    {"nombre":"avecrem de verdura", "nutriscore":"d", "alergenos":"huevos, soja, apio"},
+    {"nombre":"cocacola zero", "nutriscore":"b", "alergenos":""},
+    {"nombre":"cocacola", "nutriscore":"e", "alergenos":""},
+]
