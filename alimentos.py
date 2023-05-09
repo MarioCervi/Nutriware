8b26df30d4af7fc7cfb06bf71fa49635ab494a8b
Víctor Santocildes Prado <victorsantocildes@opendeusto.es>
2023-05-09 16:51:18 +0200

    actualizacion BBDD

--- alimentos.py
+++ alimentos.py
@@ -4,4 +4,8 @@ alimentos = [
     {"nombre":"nutella", "nutriscore":"e", "alergenos":"leche, soja, frutos secos"},
     {"nombre":"avecrem de verdura", "nutriscore":"d", "alergenos":"huevos, soja, apio"},
     {"nombre":"cocacola zero", "nutriscore":"b", "alergenos":""},
-    
+    {"nombre":"cocacola", "nutriscore":"e", "alergenos":""},
+    {"nombre":"corn flakes", "nutriscore":"b", "alergenos":"gluten, frutos secos, cacahuetes, soja, sesamo"},
+    {"nombre":"yogur natural", "nutriscore":"a", "alergenos":"leche"},
+    {"nombre":"yogur griego", "nutriscore":"c", "alergenos":"leche"},
+]
