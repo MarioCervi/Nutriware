75540cba5122e4333cc3296385fa5962612ffb92
Javier Moreno <javiermoreno@opendeusto.es>
2023-05-10 23:23:53 +0200

    avance en similitud de productos

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
-from flask import Flask, render_template, json
+from flask import Flask, render_template, jsonify, request
+from alimentos import alimentos2
 
 app = Flask(__name__)
 
 @app.route("/main")
 def main():
     return render_template("index.html")
+
+@app.route("/buscador/", methods=["GET", "POST"])
+def buscador():
+    if request.method == "POST":
+        alimento = request.form["buscador"]
+        for a in alimentos2:
+            if a["nombre"] == alimento:
+                nutriscore = a["nutriscore"]
+                return nutriscore
+    return render_template("buscador.html")
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
--- similitudes.py
+++ similitudes.py
@@ -1,32 +1,40 @@
-from hermetrics.hamming import Hamming # instalar con pip install hermetrics
+from hermetrics.hamming import Hamming
 import alimentos
 
-# probando modulo hermetrics 
-
-s1 = ["chocolate",
-      "coca-cola",
-      "caracoles",
-      "conejo",
-      "cacaolat",
-      "arroz"]
-
-busqueda_usuario = "actimel"
+busqueda_usuario = "ajos" # provisional hasta que se vincule con la busqueda del usuario real 
 
 listadoAlimentos = []
 
-for element in alimentos.alimentos2:
+for element in alimentos.alimentos2: # Para conseguir un listado solo con los nombres 
 
-    s3 = len(alimentos.alimentos2) # ver la longitud del diccionario
-    #print(s3)
     listado = (element["nombre"])
     listadoAlimentos.append(listado)
     
-
-    #print("el listado es: "+listado) # 
-
-
-print(listadoAlimentos)
-
+#print(listadoAlimentos) # verificar listado de bbdd 
 
 ham = Hamming()
-print("La palabra mas similar es: "+max(listadoAlimentos, key=lambda cadena:ham.similarity(cadena, busqueda_usuario)))
+
+similitudes = []
+
+for producto in listadoAlimentos:
+
+    similitud = ham.similarity(producto, busqueda_usuario) # devuelve una cifra de similitud
+    #print(similitud)
+    similitudes.append((producto, similitud))
+    #print(similitudes)
+
+# reverse=True porque sino se ordena automaticamente de menos similar a mas similar
+# similitudes muestra el nombre del producto y su cifra de similitud, nos interesa
+# solo la parte numerica para ordenarla
+
+similitudes_ordenadas = sorted(similitudes, key=lambda cifra_similitud: cifra_similitud[1], reverse=True) # ordenar de mayor similitud a menos
+
+
+tres_repeticiones = 0
+
+while tres_repeticiones<3: # MOSTRAR LOS 3 PRODUCTOS MAS PARECIDOS AL INTRODUCIDO POR EL USUARIO
+
+    mostrar_productos=(similitudes_ordenadas[tres_repeticiones])
+    print("PRODUCTO SIMILAR: ")
+    print(mostrar_productos)
+    tres_repeticiones +=1
