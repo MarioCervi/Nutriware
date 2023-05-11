be5c131a79753901adc521d0f36815d0c0c0ec4a
Mikel Pablos <mikel.pablos@opendeusto.es>
2023-05-11 13:28:29 +0200

    Creada autenticación contra Google.

--- app.py
+++ app.py
@@ -1,11 +1,29 @@
-from flask import Flask, render_template, jsonify, request
+import os
+os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
+
+from flask import Flask, render_template, jsonify, request, url_for, redirect
+from flask_dance.contrib.google import make_google_blueprint, google
 from alimentos import alimentos
 
 app = Flask(__name__)
 
+#Configuración para inicio de sesión con google:
+app.secret_key = "nutriware-secret-key"
+blueprint = make_google_blueprint(
+    client_id="534885315451-nlatqmtcrsvboq3jjjntgik1u71364g9.apps.googleusercontent.com",
+    client_secret="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",
+    scope=["openid", "https://www.googleapis.com/auth/userinfo.email", "https://www.googleapis.com/auth/userinfo.profile"]
+)
+app.register_blueprint(blueprint, url_prefix="/login")
+
 @app.route("/")
-def main():
-    return render_template("index.html")
+def index():
+    if not google.authorized:
+        return render_template("index.html")
+    else:
+        resp = google.get("/oauth2/v2/userinfo")
+        assert resp.ok, resp.text
+        return render_template("buscador.html", email=resp.json()["email"])
 
 @app.route("/buscador/", methods=["GET", "POST"])
 def buscador():
