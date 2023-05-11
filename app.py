84bf6aaf8a32d980b8e4653c3174326d12843e96
Javier Moreno <javiermoreno@opendeusto.es>
2023-05-12 00:19:31 +0200

    similitudes nuevos avances

--- app.py
+++ app.py
@@ -7,14 +7,25 @@ app = Flask(__name__)
 def main():
     return render_template("index.html")
 
+
 @app.route("/buscador/", methods=["GET", "POST"])
 def buscador():
     if request.method == "POST":
         alimento = request.form["buscador"]
+<<<<<<< Updated upstream
         for a in alimentos2:
             if a["nombre"] == alimento:
                 nutriscore = a["nutriscore"]
                 return nutriscore
+=======
+        dict = similitudes.mostrar_similitudes(alimento) # aqui pasarle el argumento "alimento"
+        #for a in alimentos:
+            #if a["nombre"] == alimento:
+                #nutriscore = a["nutriscore"]
+                #return nutriscore
+        return dict
+    #return jsonify()
+>>>>>>> Stashed changes
     return render_template("buscador.html")
 
 if __name__ == "__main__":
