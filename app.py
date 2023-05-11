084d255d8e15c7a9928510dc6e93c4c4a95d6fdf
Javier Moreno <javiermoreno@opendeusto.es>
2023-05-12 00:27:02 +0200

    arreglado

--- app.py
+++ app.py
@@ -1,36 +1,43 @@
-from flask import Flask, render_template, jsonify, request
-from alimentos import alimentos2
+import os
+os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
+
+from flask import Flask, render_template, jsonify, request, url_for, redirect
+from flask_dance.contrib.google import make_google_blueprint, google
+from alimentos import alimentos
+import similitudes
 
 app = Flask(__name__)
 
-@app.route("/main")
-def main():
-    return render_template("index.html")
+#Configuración para inicio de sesión con google:
+app.secret_key = "nutriware-secret-key"
+blueprint = make_google_blueprint(
+    client_id="534885315451-nlatqmtcrsvboq3jjjntgik1u71364g9.apps.googleusercontent.com",
+    client_secret="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",
+    scope=["openid", "https://www.googleapis.com/auth/userinfo.email", "https://www.googleapis.com/auth/userinfo.profile"]
+)
+app.register_blueprint(blueprint, url_prefix="/login")
 
+@app.route("/")
+def index():
+    if not google.authorized:
+        return render_template("index.html")
+    else:
+        resp = google.get("/oauth2/v2/userinfo")
+        assert resp.ok, resp.text
+        return render_template("buscador.html", email=resp.json()["email"])
 
 @app.route("/buscador/", methods=["GET", "POST"])
 def buscador():
     if request.method == "POST":
         alimento = request.form["buscador"]
-<<<<<<< Updated upstream
-        for a in alimentos2:
+        #dict = similitudes.mostrar_similitudes(alimento) # aqui pasarle el argumento "alimento"
+        for a in alimentos:
             if a["nombre"] == alimento:
                 nutriscore = a["nutriscore"]
-                return nutriscore
-=======
-        dict = similitudes.mostrar_similitudes(alimento) # aqui pasarle el argumento "alimento"
-        #for a in alimentos:
-            #if a["nombre"] == alimento:
-                #nutriscore = a["nutriscore"]
-                #return nutriscore
-        return dict
-    #return jsonify()
->>>>>>> Stashed changes
+                alergenos = a["alergenos"]
+                return render_template("resultados.html", nutriscore=nutriscore, alergenos=alergenos, a=alimento)
+            
     return render_template("buscador.html")
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
-
-
-
-#prueba
