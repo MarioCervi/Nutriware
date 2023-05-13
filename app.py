01d0a31129a4d9e7472c0f4304ab8d0a80cf7cf4
MarioCervi <mariocervilla@opendeusto.es>
2023-05-13 17:46:21 +0200

    commit para recoger lo de main

--- app.py
+++ app.py
@@ -1,31 +1,31 @@
-import os
-os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
+# import os
+# os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
 
 from flask import Flask, render_template, jsonify, request, url_for, redirect
-from flask_dance.contrib.google import make_google_blueprint, google
+# from flask_dance.contrib.google import make_google_blueprint, google
 from alimentos import alimentos
 import similitudes
 
 app = Flask(__name__)
 
 # Configuración para inicio de sesión con google:
-app.secret_key = "nutriware-secret-key"
-blueprint = make_google_blueprint(
-    client_id="534885315451-nlatqmtcrsvboq3jjjntgik1u71364g9.apps.googleusercontent.com",
-    client_secret="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",
-    scope=["openid", "https://www.googleapis.com/auth/userinfo.email", "https://www.googleapis.com/auth/userinfo.profile"]
-)
-app.register_blueprint(blueprint, url_prefix="/login")
+# app.secret_key = "nutriware-secret-key"
+# blueprint = make_google_blueprint(
+#     client_id="534885315451-nlatqmtcrsvboq3jjjntgik1u71364g9.apps.googleusercontent.com",
+#     client_secret="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",
+#     scope=["openid", "https://www.googleapis.com/auth/userinfo.email", "https://www.googleapis.com/auth/userinfo.profile"]
+# )
+# app.register_blueprint(blueprint, url_prefix="/login")
 
-@app.route("/")
-def index():
-    if not google.authorized:
-        return render_template("index.html")
-    else:
-        resp = google.get("/oauth2/v2/userinfo")
-        if resp.ok =="true":
-            return "bien"
-    return render_template("buscador.html")
+# @app.route("/")
+# def index():
+#     if not google.authorized:
+#         return render_template("index.html")
+#     else:
+#         resp = google.get("/oauth2/v2/userinfo")
+#         if resp.ok =="true":
+#             return "bien"
+#     return render_template("buscador.html")
 
 @app.route("/buscador/", methods=["GET", "POST"])
 def buscador():
