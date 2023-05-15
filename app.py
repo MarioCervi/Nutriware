baaed70889840df42f3789f2fcb3a000e7fb8f2c
MarioCervi <mariocervilla@opendeusto.es>
2023-05-15 17:27:44 +0200

    app.py comentado

--- app.py
+++ app.py
@@ -31,52 +31,59 @@ def google():
         }
     )
 
-    # Redirect to google_auth function
+    # Se redirige a google_auth function
     redirect_uri = url_for('google_auth', _external=True)
     print(redirect_uri)
     return oauth.google.authorize_redirect(redirect_uri)
 
 @app.route('/google/auth')
 def google_auth():
-    #recuperar token y toda la sesión
+
+    # Se recupera el token y toda los datos de la sesion de Google
     token = oauth.google.authorize_access_token()
     user_info = token
     user_email = token['userinfo']['email']
     user_name = token['userinfo']['name']
-    #guardar en sesion
+
+    # Se guardan los datos en la sesion de la aplicacion
     session['email'] = user_email
     session['name'] = user_name
     session['info'] = user_info
+    
+    # Se redirige al buscador
     return redirect('/buscador/')
-
 
 @app.route("/buscador/", methods=["GET", "POST"])
 def buscador():
-    #recupero el nombre de la sesión
+    # Se recupera el nombre de la sesion
     if 'name' not in session:
-        # si no tiene el nombre signigica que no ha iniciado sesión
-        # se le redirige a el login
+
+        # Si no tiene el nombre significa que no ha iniciado sesion por lo que se redirige al login
         return redirect('/')
     nombre = session['name']
-    
-    # atiende los métodos
     if request.method == "POST":
+
+        # Se recoge el valor que se ha escrito en el campo de busqueda del formulario que se ha mandado con la peticion post
         alimento = request.form["buscador"]
-        
-        
+
+        # Se comprueba que no se ha escrito ningun numero. En caso de que se escriba alguno se muestra un mensaje de error
         if alimento.isdigit():
             error_message = "Error, no puedes introducir numeros"
             return render_template("buscador.html", nombre=nombre, error_message_1=error_message)
+        
+        # Se comprueba que no se ha escrito ningun caracter especial. En caso de que se escriba alguno se muestra un mensaje de error
         elif re.search(r"[^a-zA-Z0-9\s]", alimento):
             error_message = "Error, no puedes introducir simbolos especiales"
             return render_template("buscador.html", nombre=nombre, error_message_2=error_message)
-
-        #  Busca los alimentos que sintacticamente mas se parezcan al que se ha buscado
-        else :
+        
+        # Una vez que se han hecho las comprobaciones pertinentes en el alimento buscado, se pasa todo a minusculas para realizar todas las busquedas
+        else:
             alimento = alimento.lower()
+
+        # Se busca los alimentos que sintacticamente mas se parezcan al que se ha buscado
         dict = funciones.mostrar_similitudes(alimento) 
 
-        # Busca tanto el nutriscore como los alergenos del producto buscado
+        # Se busca tanto el nutriscore como los alergenos del producto buscado
         answer = funciones.resultado_busqueda(alimento, dict)
         
         return answer, 200
